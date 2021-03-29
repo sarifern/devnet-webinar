d38659fce27cd937cd02ccc4c9853ecd17299fd2
Sari Fernandez <sarifern@cisco.com>
2021-03-29 16:38:43 -0600

    [sarifern] taking out duplicate post

--- api_use_pillar/create_message.py
+++ api_use_pillar/create_message.py
@@ -1,24 +1,14 @@
 import requests,json
 
 # Get access token from a bot
-ACCESS_TOKEN = "ODJkN2QyYjYtZmJkNy00Yzk0LTk0NmMtNDQxODNjYTU2YTE0NmUwNWQ4MTYtYjQ2_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+ACCESS_TOKEN = "NDE5ZWNkYTEtYTcxMi00NWRjLWIxNDktZTNlNzZiNzdiMjEwOTViNGJkM2MtMGFj_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
 # Get the roomID from https://developer.webex.com/docs/api/v1/rooms/list-rooms
 roomID = "Y2lzY29zcGFyazovL3VzL1JPT00vN2I3NTRiNjItNzE3Yi0zMmYzLWJiNmUtNDVjNjlkNGRkYjdk"
 
 m = {
     "roomId": roomID,
-    "text": "Este es un test"
+    "text": "Este es un test desde webinar de Devnet"
 }
-
-r = requests.post(
-    "https://webexapis.com/v1/messages",
-    data=json.dumps(m),
-    headers={
-        "Authorization": "Bearer 234{}".format(ACCESS_TOKEN),
-        "Content-Type": "application/json",
-    },
-)
-print(r.text)
 
 r = requests.post(
     "https://webexapis.com/v1/messages",
@@ -29,3 +19,5 @@ r = requests.post(
     },
 )
 print(r.text)
+
+
